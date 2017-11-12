e4dcea267847c1409254e6f8d217d20620c207fe
Unknown <joseeduardo@megasoftgyn.com.br>
2017-11-12 03:03:22 -0200

    [#02] Implementação de conexão com MongoDB
    -Separação das configurações do servidor para arquivo server.js
    -Criação de conexão com mongodb

---
+++ config/database.js
@@ -0,0 +1,2 @@
+const mongoose = require('mongoose');
+module.exports = mongoose.createConnection('mongodb://localhost/db_finance');
--- loader.js
+++ loader.js
@@ -1,12 +1,2 @@
-const port = 3003;
-
-const bodyParser = require('body-parser');
-const express = require('express');
-const app = express();
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-app.listen(port, function() {
-  console.log(`BACKEND is running on port ${port}`);
-});
+require('./config/server');
+require('./config/database');
