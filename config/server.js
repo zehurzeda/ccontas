5a6ac7998cfd603ec3b7b0fb40410b4af35c4ee3
Unknown <joseeduardo@megasoftgyn.com.br>
2017-11-23 00:07:42 -0200

    Habilitando CORS

--- config/server.js
+++ config/server.js
@@ -3,9 +3,11 @@ const port = 3003;
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+const allowCors = require('./cors');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(allowCors);
 
 app.listen(port, function() {
   console.log(`BACKEND is running on port ${port}`);
