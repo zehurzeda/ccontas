3da88394a20781bbaf3bc97e3bcd8bc2669220ef
Unknown <joseeduardo@megasoftgyn.com.br>
2018-01-05 21:12:09 -0200

    Implantação da autenticação no backend

--- config/routes.js
+++ config/routes.js
@@ -1,16 +1,28 @@
-const express = require('express');
+const express = require('express')
+const auth = require('./auth')
+module.exports = function (server) {
+  /*
+  * Rotas abertas
+  */
+  const openApi = express.Router()
+  server.use('/oapi', openApi)
 
-module.exports = function(app) {
+  const AuthService = require('../api/user/authService')
+  openApi.post('/login', AuthService.login)
+  openApi.post('/signup', AuthService.signup)
+  openApi.post('/validateToken', AuthService.validateToken)
 
+  /*
+  * Rotas protegidas por Token JWT
+  */
+  const protectedApi = express.Router()
+  server.use('/api', protectedApi)
 
-  //API Routes
-  const router = express.Router();
-  app.use('/api', router);
+  protectedApi.use(auth)
 
-  const billingCycleService = require('../api/billingCycle/billingCycleService');
-  billingCycleService.register(router, '/billingCycles');
+  const billingCycleService = require('../api/billingCycle/billingCycleService')
+  billingCycleService.register(protectedApi, '/billingCycles')
 
-  const billingSummaryService = require('../api/billingSummary/billingSummaryService');
-  router.route('/billingSummary').get(billingSummaryService.getSummary);
-
-};
+  const billingSummaryService = require('../api/billingSummary/billingSummaryService')
+  protectedApi.route('/billingSummary').get(billingSummaryService.getSummary)
+}
